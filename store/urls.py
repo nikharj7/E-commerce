c29cde29a945355411caa10189fa398af61a669f
nikharj7 <nikharj@gmail.com>
2020-12-22 23:19:18 +0530

    Ecom commit

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from store import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    
+    path('update_item/', views.updateItem, name='update_item'),
+    path('process_order/', views.processOrder, name='process_order'),
+]
